9591f43435c3599f4bc44a3cc2b471c86202b458
Waldemar <w.glaz@cheil.com>
2024-10-31 17:06:07 +0100

    16:38

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+
+import "@/assets/styles/globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Bookit App | Book a room",
+  description: "Book a meeting or conference room for your team",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
